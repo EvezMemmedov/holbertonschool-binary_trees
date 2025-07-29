d9b6d93451bb8da5b76176091e1af09cad69b277
Evez Memmedov <evezm254@gmail.com>
2025-07-29 12:33:49 +0000

    Added

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -34,7 +34,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_node->right = NULL;
 	}
 	new_node->left = NULL;
-	new_node->right = new_node;
+	parent->right = new_node;
 
 	return (new_node);
 }
