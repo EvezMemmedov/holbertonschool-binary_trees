c19ca5670a3eb4443830760423de5747a8eac105
Evez Memmedov <evezm254@gmail.com>
2025-07-29 13:35:24 +0000

    Added

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "binary_trees.h"
-
+/**
+ * binary_tree_preorder - tree using pre-order traversal
+ * @tree: pointer
+ * @func: function
+ *
+ * Return: Nothing
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL && func != NULL)
