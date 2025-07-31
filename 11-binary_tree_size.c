71d8aa6369ff71a5fd69900476eb5e4a812d85bc
Evez Memmedov <evezm254@gmail.com>
2025-07-31 08:14:03 +0000

    Added

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -15,8 +15,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+
 	left_size = binary_tree_size(tree->left);
 	right_size = binary_tree_size(tree->right);
 
