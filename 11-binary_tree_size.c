3ea28130a45202eaa3af90cfcafa2e0e68846028
Evez Memmedov <evezm254@gmail.com>
2025-07-31 08:08:15 +0000

    Added

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree-size - the size of a binary tree
+ * @tree: pointer
+ *
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size);
+}
