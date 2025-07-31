f97def6189b5d8c165904c78bdbb8e5fb82276de
Evez Memmedov <evezm254@gmail.com>
2025-07-31 09:23:57 +0000

    Added

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,16 +10,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-        size_t left_child = 0;
-      size_t right_child = 0;
+	size_t left_child = 0;
+	size_t right_child = 0;
 
-        if (tree == NULL)
-                return (0);
-        if (tree->left == NULL && tree->right == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-        left_child = binary_tree_nodes(tree->left);
-        right_child = binary_tree_nodes(tree->right);
+	left_child = binary_tree_nodes(tree->left);
+	right_child = binary_tree_nodes(tree->right);
 
-        return (left_child + right_child + 1);
+	return (left_child + right_child + 1);
 }
