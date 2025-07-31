8c7449c9e8a26d448ee066336c4a8f9b27a6c1f7
Evez Memmedov <evezm254@gmail.com>
2025-07-31 06:47:37 +0000

    Added

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,19 +10,15 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_depth;
-	size_t right_depth;
+	size_t depth = 0;
 
 	if (tree == NULL)
-		return (0);
-	
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	left_depth = binary_tree_depth(tree->left);
-	right_depth = binary_tree_depth(tree->right);
+		 return (0);
 
-	if (left_depth > right_depth)
-		return (left_depth);
-	else
-		return (right_depth);
+	 while (tree->parent)
+	 {
+		 tree = tree->parent;
+		 depth++;
+	 }
+	 return (depth);
 }
