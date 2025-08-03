042b712a92643614cd776a6fb4bb8e9ef36274d1
Evez Memmedov <evezm254@gmail.com>
2025-08-03 10:04:15 +0000

    Added

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,7 +40,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+		return (0);
 	if (tree->left == NULL || tree->right == NULL)
 		return (1);
 
